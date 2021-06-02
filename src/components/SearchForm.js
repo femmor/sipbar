fe5f4b0bb4ff52e1853d6c80a5bdb2eb5bec7796
Emmanuel Egomson <fegomson@gmail.com>
2021-06-02 17:05:54 -0500

    Implemented the Search FUnctionality with useRef

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import {useGlobalContext} from "../context"
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext()
+  const searchValue = useRef('')
+
+  const searchValueChange = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
   return (
     <section className="section search">
-      Search form
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail drinks</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchValueChange}/>
+        </div>
+      </form>
     </section>
   )
 }
