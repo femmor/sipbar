3621b3da9d9485872c4768f15b2bd9794b3f5511
Emmanuel Egomson <fegomson@gmail.com>
2021-06-02 16:55:45 -0500

    Setup Context and Fetch drinks

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Drink = () => {
+const Drink = ({ id, name, image, glass, info }) => {
   return (
-    <div>
-      Drink component
-    </div>
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/drink/${id}`} className="btn btn-primary btn-details">
+          more details
+        </Link>
+      </div>
+    </article>
   )
 }
 
--- src/components/DrinksList.js
+++ src/components/DrinksList.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {useGlobalContext} from "../context"
+import Loader from './Loader'
+import Drink from './Drink'
 
 const DrinksList = () => {
+  const {loading, cocktails} = useGlobalContext()
+  
+  if (loading) {
+    return (
+      <Loader />
+    )
+  }
+
+  if (cocktails.length < 1) {
+    return (<h2 className="section-title">no cocktail drinks matched your search criteria</h2>)
+  }
+
   return (
     <section className="section">
       <h2 className="section-title">sipbar lounge cocktails</h2>
-      <div className="cocktails-center"></div>
+      <div className="cocktails-center">
+        {cocktails.map(drink => {
+          return (
+            <Drink key={drink.id} {...drink}/>
+          )
+        })}
+      </div>
     </section>
   )
 }
